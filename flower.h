6b67a96056180b5e153b95d24eecb720a2c7610c
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-03-21 08:31:18 -0600

    Update flower.h

--- flower.h
+++ flower.h
@@ -4,12 +4,14 @@ class flower {
 private:
 	int xpos;
 	int ypos;
-	int color[3];
+	int color[9];
 	sf::CircleShape petal;
+	sf::CircleShape petal1;
+	sf::CircleShape petal2;
 	sf::RectangleShape stem;
 public:
-	flower(int x, int y, int c[3]);
+	flower(int x, int y);
 	void draw(sf::RenderWindow& window);
-	void randomcolor(int c[3]);
+	void randomcolor();
 };
 
